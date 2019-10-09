3d04871a91a8039af28c04dfacc3eadb22d81634
Mykyta Balabuiev <1nikita.bal.2200@gmail.com>
2019-10-09 15:48:08 +0200

    add sfml libs to the project

--- Legend_of_Yve/Legend_of_Yve.cpp
+++ Legend_of_Yve/Legend_of_Yve.cpp
@@ -1,20 +1,27 @@
 // Legend_of_Yve.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-
+#include <SFML/Graphics.hpp>
 int main()
 {
-    std::cout << "Hello World!\n";
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) {
+				window.close();
+			}
+		}
+		window.clear();
+
+
+		window.display();
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
